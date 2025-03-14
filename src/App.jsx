f1564d82ec8ec193de8bd874f3ff9efe6ee50f77
vitor <stecks10@gmail.com>
2025-03-14 10:49:40 -0300

    adiciona suporte ao Tailwind CSS e remove estilos CSS antigos

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import React from "react";
 
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-
+const App = () => {
   return (
-    <div className='card'>
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '❤️' : '🤍'}
-      </button>
+    <div>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
-}
+  );
+};
 
-export default function App() {
-  return (
-    <div className="card-container">
-      <Card title="Card 1" />
-      <Card title="Card 2" />
-      <Card title="Card 3" />
-    </div>
-  )
-}
+export default App;
