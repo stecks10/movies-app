2794f66d247a063f7962593f0aa719b2b0f9c55c
vitor <stecks10@gmail.com>
2025-03-18 07:09:59 -0300

    refatora componente MovieCard para desestruturar propriedades e melhora a exibição de informações do filme

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,30 @@
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({
+  movie: { title, vote_average, poster_path, release_date, original_language },
+}) => {
   return (
-    <div className="movie-card mt-8">
+    <div className="movie-card">
       <img
-        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-        alt={movie.title}
+        src={
+          poster_path
+            ? "https://image.tmdb.org/t/p/w500" + poster_path
+            : "/no-movie.png"
+        }
+        alt=""
       />
-      <h3>{movie.title}</h3>
-      <div className="content">
-        <div className="rating">
-          <img src="star.svg" alt="star" />
-          <p>{movie.vote_average}</p>
+      <div className="mt-4">
+        <h3>{title}</h3>
+        <div className="contet">
+          <div className="rating">
+            <img src="star.svg" alt="Star Icon" />
+            <p>{vote_average ? vote_average.toFixed(1) : "0.1"}</p>
+            <span className="text-white">•</span>
+            <p className="lang uppercase">{original_language}</p>
+            <span className="text-white">•</span>
+            <p className="date">
+              {release_date ? release_date.split("-")[0] : "-"}
+            </p>
+          </div>
         </div>
-        <span>{movie.release_date.slice(0, 4)}</span>
-        <span className="lang">{movie.original_language.toUpperCase()}</span>
       </div>
     </div>
   );
